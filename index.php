db027fa8e62c7139c4f55c203c3420d829a33660
amreddys <amreddys@users.noreply.github.com>
2014-10-05 11:41:00 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCType html>
+<html>
+<head>
+  <title>Simple User Authentication</title>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+</body>
+</html>
