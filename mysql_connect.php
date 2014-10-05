4805f63ed84f10a7d84fba7833cfc99395e76956
amreddys <amreddys@users.noreply.github.com>
2014-10-05 10:06:08 -0700

    Create mysql_connect.php

---
+++ mysql_connect.php
@@ -0,0 +1,37 @@
+<?php
+
+/*  
+    Amarnath Maddireddy 
+    http://www.amreddys.com
+*/
+   
+$db_host = "localhost";
+$db_username = "UserName";
+$db_password = "PassWord";
+$db_database = "DataBase";
+
+//================ The traditional way ================
+/* This method is given just for reference and it is not at all preferable to use this in real time as it is vulnerable
+$con = mysql_connect($db_host, $db_username, $db_password); //Connect to the database
+if ($con)
+{
+    $selDb = mysql_select_db($db_database, $con);
+    if(!$selDb)
+    {
+        die("Unable to select database : ".mysql_error());
+    }
+}
+else
+{
+    die('Could not connect: ' . mysql_error());
+}
+*/
+
+//================ The structural way ================
+// Here i in the MySQLi stands for improved.
+$con=mysqli_connect($db_host, $db_username, $db_password,$db_database);
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
